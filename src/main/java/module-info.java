9b047a23e83a95119fd04dfa86104be9420cf034
thanhtamn1102 <thanhtamr1102@gmail.com>
2023-08-18 17:34:45 +0700

    Fashion Shop Project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,28 @@
+module com.example.fashionshop {
+    requires java.naming;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.hibernate.orm.core;
+    requires jakarta.persistence;
+    requires guava;
+    requires poi;
+    requires static lombok;
+    requires com.google.gson;
+    requires java.json;
+
+    opens com.example.fashionshop to javafx.fxml;
+    exports com.example.fashionshop;
+
+    opens com.example.fashionshop.model to org.hibernate.orm.core, com.google.gson;
+    exports com.example.fashionshop.model;
+
+    exports com.example.fashionshop.controller;
+    opens com.example.fashionshop.controller to javafx.fxml;
+
+    exports com.example.fashionshop.cus_comp;
+    opens com.example.fashionshop.cus_comp to javafx.fxml;
+
+    exports com.example.fashionshop.dao;
+    opens com.example.fashionshop.dao to javafx.fxml;
+
+}
